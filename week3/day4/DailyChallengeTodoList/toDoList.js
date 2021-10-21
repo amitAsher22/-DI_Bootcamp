b63cc7c229a3a6b0152676c5a8e63fc6e81f6269
amita <amitasher4@gmail.com>
2021-10-21 13:14:59 +0300

    fix todo-list

--- week3/day4/DailyChallengeTodoList/toDoList.js
+++ week3/day4/DailyChallengeTodoList/toDoList.js
@@ -34,21 +34,63 @@ ul.style.fontSize = "30px"
 
 
 //// action
-button.addEventListener("click" , addToList)
+button.addEventListener("click" , checkInput)
 
 
 function addToList(){
   let li = document.createElement("li");
-  let valueInput = input.value
-
-  li.textContent = valueInput
- 
-  li.style.listStyle = "none"
-  li.style.backgroundColor = "white"
-   li.style.textAlign = "left"
-  li.style.paddingLeft = "10px"
+  let div = document.createElement("div")
+  let checkbox = document.createElement("INPUT")
+  let buttonX = document.createElement("button")
+  let btnText = document.createTextNode('X')
+  let valueInput = document.createTextNode(input.value)
   
-    ul.appendChild(li)
-    input.value = " "
-    
+
+
+  checkbox.setAttribute("type" , "checkbox")
+
+
+  checkbox.addEventListener("change", check)
+  function check (){
+    if(checkbox.checked === true){
+        li.style.textDecorationLine = "line-through"
+
+    } else if (checkbox.checked === false) {
+      li.style.textDecorationLine = "none"
+    }
 }
+  div.setAttribute("class" , "BTNdiv")
+ 
+
+   li.style.listStyle = "none"
+   li.style.backgroundColor = "white"
+   li.style.textAlign = "left"
+   li.style.paddingLeft = "10px"
+   li.style.borderBottom = "1px solid black"
+   div.style.backgroundColor = "white"
+   
+   
+
+   ul.appendChild(li)
+   li.appendChild(div)
+   li.appendChild(valueInput)
+   buttonX.appendChild(btnText)
+  div.appendChild(buttonX)
+  div.appendChild(checkbox)
+ 
+    input.value = ""
+    buttonX.addEventListener("click" , function(event){
+      console.log(li.remove());
+    })
+}
+
+function checkInput (){
+  if(input.value === ""){
+      alert('your task is empty , full the input')
+      return
+  } else {
+      addToList()
+  }
+}
+
+
