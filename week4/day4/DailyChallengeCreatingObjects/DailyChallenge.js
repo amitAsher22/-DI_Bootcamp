46c68646ac9b45e85e84d7d44fd8a1f5a4f637fe
amita <amitasher4@gmail.com>
2021-10-28 11:38:08 +0300

    fix daily challenge

--- week4/day4/DailyChallengeCreatingObjects/DailyChallenge.js
+++ week4/day4/DailyChallengeCreatingObjects/DailyChallenge.js
@@ -1,22 +1,55 @@
 
+// What You Will Learn :
+
+// Create Objects
+
+
+// Instructions
+// In this exercise, you will use object oriented programming concepts to define and use a custom object in JavaScript.
+
+// Create a class named Video. The class should be constructed with the following parameters:
+// title (a string)
+// uploader (a string, the person who uploaded it)
+// time (a number, the duration of the video - in seconds)
+// Create a method called watch() which displays a string as follows:
+// “uploader parameter watched all time parameter of title parameter!”
+// Instantiate a new Video instance and call the watch() method.
+// Instantiate a second Video instance with different values.
+// Bonus: Use an array to store data for five Video instances (ie. title, uploader, time)
+// Think of the best data structure to save this information within the array.
+// Bonus: Loop through the array to instantiate those instances.
+
+
+
+
 class Video{
     constructor(title , uploader , time){
         this.title = title;
         this.uploader = uploader;
         this.time = time;
+        
+
        
     }
     watch(){
         console.log(`${this.uploader} watched all ${this.time} of ${this.title}!`);
     }
-}
-
-class NewVideo extends Video{
-    constructor(title , uploader , time){
-     super(title , uploader , time)
+    arrayDetails(){
+       let newArray = []
+       newArray.push(this.time , this.uploader , this.time)
+        console.log(newArray);
     }
 }
 
-let  video1 = new Video("javascript" , "amit" , "44 min")
+
+
+let video1 = new Video("javascript" , "amit" , "44 min")
+let video2 = new Video("Python" , "ran" , "30 min")
+
+// Instantiate a new Video instance and call the watch() method.
+video1.watch()
+video1.arrayDetails()
+
 
 console.log(video1);
+// console.log(video2);
