183e8c1cf6ceaf24a3683ec5a9d77a6cfa2b1ae2
amita <amitasher4@gmail.com>
2021-10-31 22:10:59 +0200

    change answer to excersiesXp

--- week5/day1/ExercisesXP/Form.js
+++ week5/day1/ExercisesXP/Form.js
@@ -23,7 +23,7 @@
 // Use the same form structure as Exercise 1.
 // When you click the Send button, the form will be submitted with a POST method.(you can use the same HTML file for the action url)
 // Where will the sent data appear? Hint : Look at the Network Tab?
-// // Answer: the data send to the server , the the data change into the server.
+// // Answer: the data send to the server , the the data add information into the server.
 
 
 /////////////////////////////////////////////////////////////////////////////////////// Exercise 3 : JSON Mario
